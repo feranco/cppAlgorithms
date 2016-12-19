61117df75efcd0e7cd24554dedc7687f60c20b48
feranco <feranco@gmail.com>
2016-12-19 23:26:15 +0100

    Add list, modify recursion

---
+++ source/list.cpp
@@ -0,0 +1,264 @@
+#include "list.h"
+#include <cstdlib> //rand
+
+List::List(int n) {
+  srand(time(0));
+  head = new Node(rand()%1000,0);
+  link t = head;
+  for(int i = 1; i < n; ++i) {
+    t->next = new Node(rand()%1000,0);
+    t = t->next;
+  }
+}
+
+List::~List() {
+  while(head != 0) {
+    link r = head;
+    head = head->next;
+    delete r;
+  }
+  head = 0;
+}
+
+List::List(const List& ml) {
+  head = new Node( ml.head->item, 0);
+  link newList = head;
+  for(link t = ml.head->next; t!=0; t=t->next) {
+    newList->next = new Node(t->item,0);
+    newList = newList->next;
+  }
+}
+
+void List::removeEven(void) {
+  link t = head;
+  while(t != 0) {
+    link r = t->next;
+    if(r == 0) break;
+    t->next = r->next;
+    t=t->next;
+    delete r;
+  }
+}
+
+int oddTest(List::link t) {
+  std::cout << t->item << " ppp " <<  (t->item) % 2 <<std::endl ;
+  return (t->item) % 2;
+}
+
+void List::modify(isOdd) {
+    Node fakeHead(0,head);
+    link t = &fakeHead;
+    while(t->next != 0) {
+        if(t->next->item % 2) {
+            link r = t->next;
+            t->next = r->next;
+            delete r;
+        }
+        else{
+            t = t->next;
+        }
+    }
+    head = fakeHead.next;
+}
+
+void List::reverse(void) {
+  link r = 0; //ptr to already reversed node
+  link nr = head; //ptr to node to be reversed
+  while(nr != 0) {
+    link t = nr->next;
+    nr->next = r;
+    r = nr;
+    nr = t;
+  }
+  head = r;
+}
+
+void List::reverseRec(void) {
+  static link r = 0;
+  static link y = head;
+  link t = y->next;
+  y->next = r;
+  r = y;
+  y = t;
+  reverseRec();
+  
+}
+/*
+void removeLastRec (void) {
+  static link t = head;
+  if (t->next == 0) {
+    delete t->next;
+    return;
+  }
+  else{
+    t = t->next;
+    removeLastRec();
+  }
+}
+*/
+void List::biggestBack(void) {
+  link biggest = head;
+  link preBiggest = 0;
+  link t = head;
+  int i = 0;
+  
+  for(t = head; t->next != 0; t = t->next) {
+    std::cout << i++ << " ";
+    if(t->next->item > biggest->item) {
+      biggest = t->next;
+      preBiggest = t;
+    }
+  }
+  t->next = biggest;
+  preBiggest->next = biggest->next;
+  biggest->next = 0;
+}
+
+void List::rearrangeOddEven(void) {
+  
+  link even = head->next;
+  link odd = head;
+  link evenHead =even;
+  int cnt = 1;
+  for(link t = even->next; t !=0; t = t->next) {
+    if(cnt%2 == 0){
+      even->next = t;
+      even = even->next;
+    }
+    else {
+      odd->next = t;
+      odd = odd->next;
+    }
+    ++cnt;
+  }
+  even->next = 0;
+  odd->next = evenHead;
+}
+
+void List::sort(void) {
+  Node fakeHeadOld(0, 0);
+  fakeHeadOld.next = head;
+  Node fakeHeadNew(0, 0);
+  link u, n;
+  for(link t = fakeHeadOld.next; t != 0; t = u) {
+    u = t->next;
+    for(n = &fakeHeadNew; n->next != 0; n = n->next) {
+      if (n->next->item > t->item) break;    
+    }
+    t->next = n->next;//append to the current node the new list
+    n->next = t;//insert current node inthe right place 
+  }
+  head = fakeHeadNew.next;
+}
+
+std::ostream& operator<<(std::ostream& out, const List& ml) {
+  List::link t = ml.head;
+  do {
+    out << t->item << " ";
+    t = t->next;
+  }
+  while(t!=0);
+
+  return out;
+}
+
+
+CircularList::CircularList(int size) {
+  n = new Node(1,0);
+  n->next = n;
+  link t = n;
+  for(int i = 2; i <= size; ++i) {
+    t->next = new Node(i,n);
+    t = t->next;
+  }
+}
+
+int CircularList::size(void) {
+  int cnt = 1;
+  for(link t = n->next; t!=n; t = t->next) ++cnt;
+  return cnt;
+  
+}
+
+CircularList::link CircularList::getNode(int p) {
+  link t = n;
+  for(int i = 1; i < p; ++i) t = t->next;
+  return t;
+  
+}
+
+void CircularList::moveNode(link t, link x) {
+  link start = t->next; //node to be moved
+  t->next = start->next; //removed node to be moved
+  link dest = x->next->next;//node preceeding where to insert
+  start->next = dest->next;
+  dest->next = start;
+
+}
+
+
+Item CircularList::selectLeader(int m) {
+
+  link t = n;
+  while(t != t->next) {
+    for(int i = 1; i < m; ++i) {
+      t = t->next;
+    }
+    link r = t->next;
+    t->next = t->next->next;
+    std::cout <<  " Del " << r->item << std::endl;
+    delete r;
+  }
+  n = t;
+  return t->item;
+}
+
+CircularList::~CircularList() {
+
+  while(n != n->next) {
+    link t = n->next;
+    n->next = t->next;
+    std::cout << "Del " << t->item << std::endl;
+    delete t;
+  }
+  
+  std::cout << "Del " << n->item << std::endl;
+  delete n;
+  n = 0;
+}
+
+ListArray::ListArray(int n) {
+  item = new Item[n];
+  next = new int[n];
+  for(int i = 0; i < n; ++i) {
+    item[i] = i+1;
+    next[i] = i+1;
+  }
+  next[n-1] = 0;
+}
+
+std::ostream& operator<<(std::ostream& out, const CircularList& mcl) {
+  CircularList::link  tmp = mcl.n;
+
+  do {
+    out << tmp->item << " ";
+    tmp = tmp->next;
+  }while(tmp != mcl.n);
+  out << std::endl;
+  return out;
+}
+
+
+Item ListArray::selectLeader(int m) {
+  int x = 8;
+  std::cout <<  next[x] << next[next[x]] <<  std::endl;
+  while(next[x]!=next[next[x]]) {
+    for(int i = 0; i < m-1; ++i) {
+      x = next[x];
+      std::cout << i <<  " " << x << std::endl;
+    }
+    std::cout << x <<  " Del " << item[next[x]] << std::endl;
+    next[x] = next[next[x]];
+  }
+  return item[next[x]];
+}
---
+++ source/list.h
@@ -0,0 +1,72 @@
+#ifndef LIST_H
+#define LIST_H
+#include <iostream>
+
+typedef int Item;
+ 
+ class List {
+   //Node definition
+   struct Node {
+     Item  item;
+     Node* next;
+     Node(Item i, Node* n) {item = i; next = n;}
+   };
+
+   typedef Node* link;
+   typedef int (*isOdd)(link);
+   link head;
+   
+ public:
+   List(int n);
+   ~List();
+   List(const List& l);
+   friend std::ostream& operator<<(std::ostream& out, const List& rhs);
+   void biggestBack(void);
+   void rearrangeOddEven(void);
+   void modify(isOdd);
+   void reverse(void);
+   void sort(void);
+   void removeEven(void);
+   void reverseRec(void);
+   //friend void removeLastRec(link t);
+   friend int oddTest(link t);
+ };
+
+int oddTest(List::link t);
+
+class CircularList {
+  
+  //Node definition
+  struct Node {
+    Item  item;
+    Node* next;
+    Node(Item i, Node* n) {item = i; next = n;}
+  };
+
+  typedef Node* link;
+  link n;
+ public:
+  
+  CircularList(int size);
+  ~CircularList();
+  int size(void);
+  link getNode(int i);
+  void moveNode(link t, link x);
+  Item selectLeader(int m);
+  friend std::ostream& operator<<(std::ostream& out, const CircularList& rhs);  
+ };
+
+
+
+
+class ListArray {
+  Item* item;
+  int*  next;
+  public:
+  ListArray(int n);
+  Item selectLeader(int m);
+  ~ListArray();
+  
+};
+
+#endif
