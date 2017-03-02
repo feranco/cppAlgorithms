8e47dad5d6c718a648ae3340199d05d43ab802fb
feranco <feranco@gmail.com>
2017-03-02 17:27:22 +0100

    Add mergesort

---
+++ source/mergesort.h
@@ -0,0 +1,16 @@
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+//mergesort sorting algorithm implementation
+//properties:
+//time complexity: O(NlogN) there are logN merge and each merge requires N comparison
+//space complexity: O(N) an extra array with the input size is needed
+//stability: the algorithm is stable if the merge is implemented as stable
+//time and space performance are independent from the input
+
+template <typename Item> 
+void merge (Item arr[], Item aux[], int l, int r) {
+  
+}
+
+#endif
